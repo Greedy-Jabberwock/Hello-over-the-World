ef2ff652ac2b43cb16e230faf2e4670c7a6a802e
Greedy_Jabberwock <greedy.jabberwock@gmail.com>
2023-03-23 21:07:48 +0200

    Reworked folders. Added endpoints for articles.

---
+++ front/src/components/account/Account.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Account = (props) => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+};
+
+export default Account;
